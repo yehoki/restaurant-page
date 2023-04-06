960642909de21157c10c15c099eb8295ba9c13d1
yehoki <patsenn2000@gmail.com>
2023-04-06 14:26:36 +0100

    Finish restaurant page

--- src/home.js
+++ src/home.js
@@ -5,10 +5,11 @@ const makeHomePage = () => {
   const main = document.createElement("main");
   const content = document.createElement("div");
   content.classList.add("home-page");
-  content.innerHTML += "<h1>Plank's fine dining</h1>"
-  content.innerHTML += "<h3>Think. Imagine. Receive.</h3>"
+  content.innerHTML += "<h1>Plank's fine dining</h1>";
+  content.innerHTML += "<h3>Think. Imagine. Receive.</h3>";
+  content.innerHTML += "<h4>Flavours, like you never experienced before</h4>";
   content.innerHTML +=
-    "<p>Flavours, like you never experienced before</p>";
+    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at eros eu sapien feugiat mattis. Maecenas sit amet lobortis orci. Quisque id sem a leo tempor dictum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent placerat ipsum vitae odio varius egestas. Aliquam sagittis, nulla quis eleifend faucibus, ante nisl pellentesque erat, et interdum velit quam at sapien. Phasellus eget lobortis mauris, et feugiat est.</p>";
   content.innerHTML +=
     "<a href='https://www.freepik.com/free-photo/golden-cutlery-with-textile-plate-dark-background-top-view_19182711.htm#query=restaurant&position=18&from_view=keyword&track=sph'>Background image by valeria_aksakova on Freepik</a>";
   main.appendChild(content);
--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,39 @@
-const makeMenuItem = (heading, ) => {
+const makeMenuItem = (heading, menuPrice, menuDescription) => {
   const menuItem = document.createElement("div");
   menuItem.classList.add("menu-item");
   const menuHeading = document.createElement("h3");
-  menuHeading.textContent = heading;
+  menuHeading.textContent = heading + " " + menuPrice;
   menuItem.append(menuHeading);
+  const description = document.createElement("p");
+  description.textContent = menuDescription;
+  menuItem.appendChild(description);
+  return menuItem;
 };
 
 const makeMenuPage = () => {
   const main = document.createElement("main");
   main.classList.add("menu-page");
-  main.innerHTML = "1234";
-
-  const salmon = document.createElement("div");
-  salmon.classList.add("menu-item");
-  const salmonHeading = document.create;
-
+  main.appendChild(
+    makeMenuItem(
+      "Salmon & Vegetables",
+      "£11.25",
+      "Fried Salmon with fresh vegetables and variety of sauces."
+    )
+  );
+  main.appendChild(
+    makeMenuItem(
+      "Cod & Fries",
+      "£8.90",
+      "U.K. style 'Fish & Chips' served with mushy peas and curry sauce."
+    )
+  );
+  main.appendChild(
+    makeMenuItem(
+      "Vegeterian Grain & Bean Bowl",
+      "£12.90",
+      "Vegetarian bowl of grains and beans, served with a variety of fresh vegetables."
+    )
+  );
   return main;
 };
 
